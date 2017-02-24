f6b7d35f4a50d630ab7fc76fce570b0d2c142a87
protonsint <protonsint@gmail.com>
2017-02-24 17:45:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var fs      = require('fs');
 var app     = express();
 var eps     = require('ejs');
 
+
+var socket = require('socket.io');
+
+var io = socket.listen(app);
+
+
 app.engine('html', require('ejs').renderFile);
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
@@ -21,3 +27,50 @@ app.use(function(err, req, res, next){
 
 app.listen(port, ip);
 console.log('Server running on ' + ip + ':' + port);
+
+
+
+
+var usernames = {};
+
+var rooms = ['Lobby'];
+
+io.sockets.on('connection', function(socket) {
+    socket.on('adduser', function(username) {
+        socket.username = username;
+        socket.room = 'Lobby';
+        usernames[username] = username;
+        socket.join('Lobby');
+        socket.emit('updatechat', 'SERVER', 'you have connected to Lobby');
+        socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', username + ' has connected to this room');
+        socket.emit('updaterooms', rooms, 'Lobby');
+    });
+
+    socket.on('create', function(room) {
+        rooms.push(room);
+        socket.emit('updaterooms', rooms, socket.room);
+    });
+
+    socket.on('sendchat', function(data) {
+        io.sockets["in"](socket.room).emit('updatechat', socket.username, data);
+    });
+
+    socket.on('switchRoom', function(newroom) {
+        var oldroom;
+        oldroom = socket.room;
+        socket.leave(socket.room);
+        socket.join(newroom);
+        socket.emit('updatechat', 'SERVER', 'you have connected to ' + newroom);
+        socket.broadcast.to(oldroom).emit('updatechat', 'SERVER', socket.username + ' has left this room');
+        socket.room = newroom;
+        socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username + ' has joined this room');
+        socket.emit('updaterooms', rooms, newroom);
+    });
+
+    socket.on('disconnect', function() {
+        delete usernames[socket.username];
+        io.sockets.emit('updateusers', usernames);
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+        socket.leave(socket.room);
+    });
+ });
